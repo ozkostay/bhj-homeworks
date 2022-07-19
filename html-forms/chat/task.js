815a1cb20bd606aad050cc0f9eb065e392b16bd9
Konstantin Ozhug <ozkostay@mail.ru>
2022-07-19 20:21:27 +0500

    forms 19-07-22  1

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -1,29 +1,32 @@
 const chatWidget = document.querySelector('.chat-widget');
+let robotText = ['aaa', 'bbb', 'ccc'];
 
 chatWidget.addEventListener('click', onclickChatWidget);
 
 function onclickChatWidget() {
-    console.log('asd');
-    console.log(chatWidget);
     chatWidget.classList.add('chat-widget_active');
 }
 
 const chatInput = document.getElementById('chat-widget__input');
-
 chatInput.addEventListener('change', onchangeChatInput);
 
 function onchangeChatInput() {
-    const time = new Date();
-    
-
+    let time = new Date();
     let message = '\
     <div class="message message_client">\
-        <div class="message__time">' + time.getHours().toString() + ':'  + time.getMinutes().toString() + '</div>\
+        <div class="message__time">' + ('0' + time.getHours()).slice(-2) + ':'  + ('0' + time.getMinutes()).slice(-2) + '</div>\
         <div class="message__text">' + chatInput.value + '</div>\
     </div>';
     const elementMessages = document.getElementById('chat-widget__messages');
-
     elementMessages.innerHTML += message;
+    chatInput.value = "";
     
-    console.log(chatInput.value);
+    time = new Date();
+    message = '\
+    <div class="message">\
+        <div class="message__time">' + ('0' + time.getHours()).slice(-2) + ':'  + ('0' + time.getMinutes()).slice(-2) + '</div>\
+        <div class="message__text">' + robotText[Math.floor(Math.random ()*3)] + '</div>\
+    </div>';
+    elementMessages.innerHTML += message;
+
 }
