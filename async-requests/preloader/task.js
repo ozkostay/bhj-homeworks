804049478cccc4442ef7cdf46d87c6bd63aeeb81
ozkostay <ozkostay@mail.ru>
2022-08-22 15:57:48 +0500

    JSBrowser 220822 - 1

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,24 +1,25 @@
 
-// Пример
-function getUser() {
-    try {
-        returnJSON.parse( localStorage.getItem('user') );
-    }catch(e) {
-        return null;
-    }
-};
-
-//================
 const xhr = new XMLHttpRequest();
-
-console.log('xhr ', xhr);
+const itemsTable = document.getElementById('items');
+const loaderImg = document.getElementById('loader');
 
 xhr.addEventListener('readystatechange', () => {
-    if ( xhr.readyState ) {
-        console.log(xhr.responseText);
-        console.log(111);
+    if ( xhr.readyState === xhr.DONE ) {
+        
+        xhrResponse = xhr.response;
+        responseValute = xhrResponse.response.Valute;
+        loaderImg.classList.remove('loader_active');
+
+        for (let num in responseValute) {
+            
+            // Вот ниже строка вроде правильно, а не работает :-(
+            //itemsTable.insertAdjacentElement('beforeend', '<div class="item"><div class="item__code">' + responseValute[num].CharCode + '</div><div class="item__value">' + responseValute[num].Value + '</div><div class="item__currency"> руб.</div></div>');
+            
+            itemsTable.innerHTML += '<div class="item"><div class="item__code">' + responseValute[num].CharCode + '</div><div class="item__value">' + responseValute[num].Value + '</div><div class="item__currency"> руб.</div></div>';
+        }
     }
 });
 
 xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/');
+xhr.responseType = 'json';
 xhr.send();
