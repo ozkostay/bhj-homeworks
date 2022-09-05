737846e171ec66422a3a0acc0f3a1317e68cf22e
ozkostay <ozkostay@mail.ru>
2022-09-05 19:49:13 +0500

    JSBro 050922 1

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,36 +1,38 @@
 const form = document.getElementById('signin__form');
 const welcome = document.getElementById('welcome');
 const signin = document.getElementById('signin');
-form.reset();
 
 if ( getCookie ('login') ) {
     signin.classList.remove('signin_active');
     wellcomeWindow( getCookie ('user_id') );
-} else {
-    const xhr = new XMLHttpRequest();
-    xhr.onload = function() {
-        if (  xhr.status === 200 ) {
-            if ( xhr.response.success ) {
-                signin.classList.remove('signin_active');
-                user_id = xhr.response.user_id;
-                wellcomeWindow( user_id );
-                // Запись в кукки
-                setCookie ('login', form.login.value.trim());
-                setCookie ('user_id', user_id );
-            } else {
-                alert('Неверный логин/пароль');
-            }
-        }
-    };
+} 
 
-    form.addEventListener( 'submit', (event) => {
-        event.preventDefault();
-        xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/auth.php');
-        const formData = new FormData(form);
-        xhr.responseType = 'json';
-        xhr.send(formData);
-    });
-}
+const xhr = new XMLHttpRequest();
+xhr.onload = function() {
+    if (  xhr.status === 200 ) {
+        if ( xhr.response.success ) {
+            signin.classList.remove('signin_active');
+            user_id = xhr.response.user_id;
+            wellcomeWindow( user_id );
+            // Запись в кукки
+            //console.log('222', form.login.value, '333');
+            setCookie ('login', form.login.value.trim());
+            setCookie ('user_id', user_id );
+        } else {
+            alert('Неверный логин/пароль');
+        }
+    }
+    form.reset();
+};
+
+form.addEventListener( 'submit', (event) => {
+    event.preventDefault();
+    xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/auth.php');
+    const formData = new FormData(form);
+    xhr.responseType = 'json';
+    xhr.send(formData);
+    
+});
 
 function wellcomeWindow (id) {
     document.getElementById('user_id').innerText = id;
